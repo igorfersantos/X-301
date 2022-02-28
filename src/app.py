d4d0438a2b291b89b7dcc5759d2fcada8ba9d9ae
Igor Fernandes <igorfercontato@gmail.com>
2022-02-27 22:51:31 -0300

    Requirements.txt updated and code cleaned

--- src/app.py
+++ src/app.py
@@ -1,8 +1,5 @@
 import discord
 import os
-import requests
-import json
-import random
 from dotenv import load_dotenv
 
 # Load bot environment variables
@@ -10,9 +7,11 @@ load_dotenv()
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Logged in as {client.user}')
+
 
 @client.event
 async def on_message(message):
@@ -22,7 +21,8 @@ async def on_message(message):
     msg = message.content
 
     if message.content.startswith('!map'):
-        map = msg.split("!map ",1)[1]
+        map = msg.split("!map ", 1)[1]
         await message.channel.send(map)
+
 
 client.run(os.getenv('TOKEN'))
