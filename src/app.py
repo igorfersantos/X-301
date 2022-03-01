eb720b8af40cc6dd9d4b7c511799bcc90518a650
Igor Fernandes <igorfercontato@gmail.com>
2022-02-28 21:14:34 -0300

    Cloudinary and env vars fix

--- src/app.py
+++ src/app.py
@@ -6,8 +6,13 @@ from dotenv import load_dotenv
 # Load bot environment variables
 load_dotenv()
 
-client = discord.Client()
+cloudinary.config(
+  cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
+  api_key = os.getenv('CLOUDINARY_API_KEY'),
+  api_secret = os.getenv('CLOUDINARY_API_SECRET')
+)
 
+client = discord.Client()
 
 @client.event
 async def on_ready():
